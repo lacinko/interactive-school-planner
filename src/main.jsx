9f54fe72a472697630d323c5b66a22d399c5e13b
Ladislav Topoľský <ladislav.topolsky@gmail.com>
2022-02-20 01:47:38 +0100

    Added VitePWA Plugin

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
 import { Pages } from "./pages/Pages";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW();
 
 ReactDOM.render(
   <Provider store={store}>
